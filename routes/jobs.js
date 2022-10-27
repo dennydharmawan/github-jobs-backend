2b88b21307167a5b99baa542cac3bb3dd6045c70
Denny Dharmawan <denny.dharmawan91@gmail.com>
2022-10-27 23:40:36 +0700

    add logic for searching items

--- routes/jobs.js
+++ routes/jobs.js
@@ -47,4 +47,55 @@ router.get('/:jobId/details', authenticateUser, async (req, res, next) => {
   }
 });
 
+// https://gist.github.com/jherax/f11d669ba286f21b7a2dcff69621eb72
+function filterArray(array, filters) {
+  const filterKeys = Object.keys(filters);
+
+  return array.filter((item) => {
+    // validates all filter criteria
+    return filterKeys.every((key) => {
+      // ignores non-function predicates
+      if (typeof filters[key] !== 'function') return true;
+      return filters[key](item[key]);
+    });
+  });
+}
+
+router.get('/search', async (req, res, next) => {
+  const page = parseInt(req.query?.page) || undefined;
+  const pageSize = parseInt(req.query?.pageSize) || undefined;
+  const filterCriteria = req?.body || null;
+
+  try {
+    const { data } = await axios.get(
+      'http://dev3.dansmultipro.co.id/api/recruitment/positions.json'
+    );
+
+    // const filters = {
+    //   size: (size) => size === 50 || size === 70,
+    //   color: (color) => ['blue', 'black'].includes(color.toLowerCase()),
+    //   locations: (locations) =>
+    //     locations.find((x) => ['JAPAN', 'USA'].includes(x.toUpperCase())),
+    //   details: (details) => details.length < 30 && details.width >= 70
+    // };
+
+    const filterFns = _.mapValues(
+      filterCriteria,
+      function (filterValue, filterKey, filterCriteria) {
+        return (objectPropertyValue) => {
+          return objectPropertyValue.includes(filterValue);
+        };
+      }
+    );
+
+    const filteredItems = filterArray(data, filterFns);
+
+    const paginatedItems = getPaginatedItems(filteredItems, page, pageSize);
+
+    res.send(paginatedItems);
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
