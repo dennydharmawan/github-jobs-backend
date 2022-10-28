de6b59bbc3c5b27f7040a87e74ccf9d47e204458
Denny Dharmawan <denny.dharmawan91@gmail.com>
2022-10-28 16:43:48 +0700

    trigger deployment

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.send('Home page');
+  res.send(process.env.CORS_ALLOWED_ORIGINS);
 });
 
 module.exports = router;
